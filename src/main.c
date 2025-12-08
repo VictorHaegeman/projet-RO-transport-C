df4155df7ce72df20d26b81141b0797b76a07f82
M8Ghost <cavaignacromain@gmail.com>
2025-12-08 20:11:11 +0100

    inclusion de la boucle "do while" des couts marginaux pour trouver la solution la plus opti
    Victor un do while c'est bien nan ou c est mieux un mail tu pense ?

--- src/main.c
+++ src/main.c
@@ -42,6 +42,7 @@ int main(int argc, char **argv)
         return 1;
     }
 
+    printf("\n=== SOLUTION DE DÉPART ===\n");
     afficher_solution(p, s);
 
     // === Construction et affichage de la base ===
@@ -54,6 +55,7 @@ int main(int argc, char **argv)
     } else {
         printf("\n>>> La base n'est PAS un arbre ! Correction nécessaire.\n");
     }
+
     if (!base_est_arbre(b, p->nb_fournisseurs, p->nb_clients)) {
 
         printf("\n>>> Correction automatique de la base...\n");
@@ -72,33 +74,40 @@ int main(int argc, char **argv)
                              p->nb_clients);
     }
     
-    // integration du proc potentiel ici 
-    // === Potentiels + coûts marginaux + marche-pied ===
+    //  Potentiels + coûts marginaux + marche-pied (boucle complète) 
     int pot_f[p->nb_fournisseurs];
     int pot_c[p->nb_clients];
 
-    calculer_potentiels(p, b, pot_f, pot_c);
-    afficher_potentiels(p, pot_f, pot_c);
-    afficher_table_couts_potentiels(p, pot_f, pot_c);
-
     int i_entree, j_entree;
-    int optimal = calculer_et_afficher_couts_marginaux(p, s,
+    int optimal = 0;
+    int iteration = 1;
+
+    do {
+        printf("\n================== ITERATION %d ==================\n", iteration++);
+
+        // 1) Potentiels 
+        calculer_potentiels(p, b, pot_f, pot_c);
+        afficher_potentiels(p, pot_f, pot_c);
+        afficher_table_couts_potentiels(p, pot_f, pot_c);
+
+        // 2) Coûts marginaux + choix de l'arête améliorante
+        optimal = calculer_et_afficher_couts_marginaux(p, s,
                                                        pot_f, pot_c,
                                                        &i_entree, &j_entree);
 
-    if (!optimal) {
-        printf("\n=== MARCHE-PIED SUR L'ARÊTE AMÉLIORANTE (F%d, C%d) ===\n",
-               i_entree, j_entree);
-        marche_pied(b, s, i_entree, j_entree);
+        // 3) Marche-pied si ce n'est pas optimal
+        if (!optimal) {
+            printf("\n=== MARCHE-PIED SUR L'ARÊTE AMÉLIORANTE (F%d, C%d) ===\n",
+                   i_entree, j_entree);
+            marche_pied(b, s, i_entree, j_entree);
 
-        printf("\n=== NOUVELLE SOLUTION APRÈS MARCHE-PIED ===\n");
-        afficher_solution(p, s);
-    } else {
-        printf("\n=== SOLUTION DÉJÀ OPTIMALE, PAS DE MARCHE-PIED ===\n");
-    }
+            printf("\n=== NOUVELLE SOLUTION APRÈS MARCHE-PIED ===\n");
+            afficher_solution(p, s);
+        } else {
+            printf("\n=== SOLUTION DÉJÀ OPTIMALE, PAS DE MARCHE-PIED ===\n");
+        }
 
-
-    
+    } while (!optimal);
 
     liberer_base(b);
     liberer_probleme(p);
